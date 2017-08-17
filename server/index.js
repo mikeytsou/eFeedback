d1522db4cc1c4c8544f738760c07066f2340b79e
Mike Tsou <mr.tsou@gmail.com>
2017-08-16 21:55:40 -0700

    Update passport configuration

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
-const express = require("express");
-      passport = require("passport");
-      GoogleStrategy = require("passport-google-oauth20").Strategy;
+const express = require('express');
+      passport = require('passport');
+      GoogleStrategy = require('passport-google-oauth20').Strategy;
+      keys = require('./config/keys');
       app = express();
 
 // APP CONFIG
 
 // PASSPORT CONFIG
 passport.use(new GoogleStrategy({
-
+  clientID: keys.GoogleClientID,
+  clientSecret: keys.GoogleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken);
 }));
 
 
