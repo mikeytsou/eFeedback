4b3149f169ba3f6ee5c8cabc42d7954fc4c7d2f0
Mike Tsou <mr.tsou@gmail.com>
2017-08-21 12:58:05 -0700

    Create App root component

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-ReactDom.render();
+import App from './components/App';
+
+ReactDom.render(<App />, document.querySelector('#root'));
