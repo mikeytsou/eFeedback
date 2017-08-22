c12bc1524c4a459e07b3ca70e8e4ac6f01073928
Mike Tsou <mr.tsou@gmail.com>
2017-08-22 15:48:38 -0700

    Access application state in the Header component by connecting action creator

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'Still deciding';
+      case false:
+        return 'I\'m logged out';
+      default:
+        return 'I\'m logged in';
+    }
+  }
+
   render() {
     return (
-      <nav className="ui inverted menu">
+      <nav className="ui inverted attached menu">
         <div className="ui container">
           <a href="" className="item">eFeedback</a>
 
           <div className="right menu">
-            <a href="" className="item">Login With Google</a>
+            <a href="" className="item">{this.renderContent()}</a>
           </div>
         </div>
       </nav>
@@ -16,4 +29,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header);
