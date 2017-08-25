39180fa50b327763d9312624a3be8b2b2416d934
Mike Tsou <mr.tsou@gmail.com>
2017-08-25 13:33:13 -0700

    Add logic for routing in production

--- server/index.js
+++ server/index.js
@@ -26,6 +26,15 @@ const billingRoutes = require('./routes/billingRoutes');
 authRoutes(app);
 billingRoutes(app);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build')); // any request that doesn't match exisiting route handlers, express will first look to serve up production assest ie. the main.js or main.css file
+
+  const path = require('path');
+  app.get('*', (req, res) => { // express will then attempt to serve up the index.html file(for routes created with react-router) if it doesn't recognize the routes in the existing route handlers
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+
 // DEFAULT ROUTE
 app.get('*', (req, res) => {
   res.send('PAGE NOT FOUND');
