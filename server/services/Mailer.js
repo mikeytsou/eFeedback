cb3b65310ff62c6da5638c5caf5d849ab887cc61
Mike Tsou <mr.tsou@gmail.com>
2017-08-28 19:32:40 -0700

    Set up Mailer constructor function

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -3,6 +3,15 @@ const helper = sendgrid.mail;
 const keys = require('../conf/keys');
 
 class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email('no-replay@efeedback.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+
 
 }
 
