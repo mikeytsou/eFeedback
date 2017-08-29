bd293d4495e7553d4169f45a351b359140ee1f3d
Mike Tsou <mr.tsou@gmail.com>
2017-08-28 19:41:36 -0700

    Create addClickTracking function in the Mailer

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -10,9 +10,26 @@ class Mailer extends helper.Mail {
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body); // addContent is a helper function from helper.Mail
+    this.addClickTracking();
+
   }
 
+  // extract each email from subdocument recipient collection
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
 
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
 }
 
 module.exports = Mailer;
