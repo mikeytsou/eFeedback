0f94d31bdfc9dff721c209076aececf8f6893ebb
Mike Tsou <mr.tsou@gmail.com>
2017-08-29 12:16:41 -0700

    Update Dashboard component by adding plus icon and using Link from react-router

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
     <div>
       Dashboard
+      <Link to="/surveys/new" className="circular ui icon button massive right floated">
+        <i className="plus icon"></i>
+      </Link>
     </div>
   );
 }
