173f9543b4234b6f79635eb7b69891b2a9baf7e5
Mike Tsou <mr.tsou@gmail.com>
2017-08-28 22:49:54 -0700

    Update the surveyTemplate

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -8,6 +8,10 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+// for dev testing purposes
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDom.render(
