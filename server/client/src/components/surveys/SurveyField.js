2bc94154fbce754c2aaabbcb34e454c1ce1c51cf
Mike Tsou <mr.tsou@gmail.com>
2017-08-30 12:40:35 -0700

    Wire form validation on SurveyField component

--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 export default (props) => {
+  console.log(props.meta)
   return (
     <div>
       <label>{props.label}</label>
       <input {...props.input} />
+      {props.meta.touched && props.meta.error}
     </div>
   );
 };
