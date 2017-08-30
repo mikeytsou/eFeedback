4c75663f1ca206193e3e881154c06a2756003217
Mike Tsou <mr.tsou@gmail.com>
2017-08-29 20:17:08 -0700

    Create SurveyField component and wire it to SurveyForm component

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+  renderFields() {
+    return (
+      <div>
+        <Field type="text" name="title" component={SurveyField} />
+      </div>
+    );
+  }
+
   render() {
     return(
       <div>
-        <form onSubmit={this.props.handleSubmit((values) => console.log(values))}>
-          <Field
-            type="text"
-            name="SurveyTitle"
-            component="input"
-          />
+        <form onSubmit={this.props.handleSubmit((val) => console.log(val))}>
+          {this.renderFields()}
           <button type="submit">Submit</button>
         </form>
       </div>
