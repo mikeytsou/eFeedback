a8b68560985e5801aa6e0f7b651be895c224d059
Mike Tsou <mr.tsou@gmail.com>
2017-08-30 22:03:43 -0700

    Refactor SurveyFormReview component to render each field

--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -4,7 +4,9 @@ export default (props) => {
   return (
     <div>
       <label>{props.label}</label>
+
       <input {...props.input} />
+
       <div id="input-error">
         {props.meta.touched && props.meta.error}
       </div>
--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,12 +1,23 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 const SurveyFormReview = (props) => {
+  const reviewFields = _.map(formFields, (field) => {
+    return (
+      <div key={field.name}>
+        <label>{field.label}</label>
+        <div>{props.formValues[field.name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
 
-
+      {reviewFields}
 
       <button className="ui red button" onClick={props.onBack}>
         Back
